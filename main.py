997e45ad55b2894696e5426b59fbf9797b2da62c
Petrevich Nikita <30439429+echo44@users.noreply.github.com>
2018-03-31 19:38:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import neuron as nrn
 import numpy as np
 import matplotlib.pyplot as plt
+import pylab
+
 
 def learn_me(coor, class_id):
     synapses[0][0] = coor[0]
@@ -35,7 +37,22 @@ def learn_me(coor, class_id):
                     s[1] += (v * delta) * s[0]
 
 
-v = 0.5
+def draw_plot():
+    for dot in dots:
+        if (dot[2] == 1):
+            plt.scatter(dot[0], dot[1], color="green")
+        if (dot[2] == 2):
+            plt.scatter(dot[0], dot[1], color="red")
+    x = np.arange(-10, 10, 1)
+    plt.plot(x, (-1 * synapses[0][1] * x) / synapses[1][1])
+    plt.grid(False)
+    labels = ["Classes sep. line", "Green 1-st class", "Red 2-nd class"]
+    pylab.legend(labels)
+    plt.show()
+
+# константы
+v = 0.5  # коэфициент обучения
+dots = []  # хранит координаты точек и их класс
 
 # создали нейрон
 neuron = nrn.Neuron()
@@ -51,31 +68,36 @@ synapses.append(y)
 
 # обучение
 learn_me([-1, -1], 2)
-plt.scatter(-1, -1)
-plt.scatter(-2, -1)
+dots.append([-1, -1, 2])
+
 learn_me([-2, -1], 2)
+dots.append([-2, -1, 2])
+
 learn_me([-2, -2], 2)
+dots.append([-2, -2, 2])
 
 learn_me([1, 1], 1)
+dots.append([1, 1, 1])
+
 learn_me([2, 2], 1)
+dots.append([2, 2, 1])
+
 learn_me([1, 0], 1)
-plt.scatter(1, 1)
-plt.scatter(2, 2)
+dots.append([1, 0, 1])
 
 
-x = np.arange(-10, 10, 1)
-plt.plot(x, (-1 * synapses[0][1] * x) / synapses[1][1])
-plt.grid(True)
-plt.show()
-
-while(True):
-    a = input("ENTER X ")
-    b = input("ENTER Y ")
+for i in range(2):
+    a = int(input("ENTER X "))
+    b = int(input("ENTER Y "))
     synapses[0][0] = a
     synapses[1][0] = b
     neuron.activation(synapses)
     print(neuron.axon)
     if(neuron.axon >= 0.99):
         print("THIS IS 2-nd CLASS")
+        dots.append([a, b, 2])
     if (neuron.axon <= 0.001 or neuron.axon == 0.5):
         print("THIS IS 1-st CLASS")
+        dots.append([a, b, 1])
+
+draw_plot()
