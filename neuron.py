5acfe7afeed79db848539d2de482d9e8ba5f48eb
Petrevich Nikita <30439429+echo44@users.noreply.github.com>
2018-04-01 12:17:05 +0300

    Update neuron.py

--- neuron.py
+++ neuron.py
@@ -5,7 +5,7 @@ import math
 class Neuron():
 
     def __init__(self):
-        self.__axon = 0
+        self.__axon = 0  # выходное значение
 
     @property
     def axon(self):
@@ -16,11 +16,13 @@ class Neuron():
         self.__axon = value
 
     def sum(self, synapses):
+        """Суммирует произведения весов и значений синопсов"""
         a = 0
         for s in synapses:
             a += float(s[0]) * float(s[1])
         return a
 
     def activation(self, synapses):
+        """Функция активации"""
         a = self.sum(synapses)
         self.__axon = 1 / (1 + math.exp(-a))
